86dc5b3bca6cd25e11bf211f599ef04a2de2de0a
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-02-03 10:09:20 +0530

    Update ContactForm.tsx

--- src/components/contacts/ContactForm.tsx
+++ src/components/contacts/ContactForm.tsx
@@ -250,14 +250,14 @@ export const ContactForm = () => {
               max="5"
               value={formData.relationship_level}
               onChange={(e) => setFormData({ ...formData, relationship_level: parseInt(e.target.value) })}
-              className="mt-3 block w-full accent-primary-500"
+              className="mt-3 block w-full [&::-webkit-slider-runnable-track]:bg-gradient-to-r [&::-webkit-slider-runnable-track]:from-red-400 [&::-webkit-slider-runnable-track]:to-green-400 [&::-moz-range-track]:bg-gradient-to-r [&::-moz-range-track]:from-red-400 [&::-moz-range-track]:to-green-400"
+              style={{
+                accentColor: `hsl(${(formData.relationship_level - 1) * 30}, 100%, 45%)`
+              }}
             />
-            <div className="mt-2 flex justify-between text-xs text-gray-500">
-              <span>Acquaintance</span>
-              <span>Colleague</span>
-              <span>Friend</span>
-              <span>Close Friend</span>
-              <span>Best Friend</span>
+            <div className="mt-2 flex justify-between text-sm text-gray-600">
+              <span>Distant</span>
+              <span>Close</span>
             </div>
           </div>
         </div>
