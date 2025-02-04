ffd5b4a046d7c23b8b4640aaa1ca8fa8e94671d5
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-02-04 17:33:35 +0530

    Add notice

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,13 +7,16 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-primary-50 to-white overflow-x-hidden">
+    <div className="min-h-screen bg-gradient-to-br from-primary-50 to-white overflow-x-hidden flex flex-col">
       <Navbar />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-4 pt-8 flex-grow">
         <div className="bg-white rounded-xl shadow-soft backdrop-blur-sm bg-white/50 p-6 sm:p-8">
           {children}
         </div>
       </main>
+      <footer className="text-center pt-2 pb-4 text-gray-600 text-sm">
+        © {new Date().getFullYear()} ajitesh gogoi
+      </footer>
     </div>
   );
 };
