86be85e2a53a0bf9a1d4afd78295b2ef8c95383f
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-02-09 23:09:33 +0530

    Revert "Shift from chrome tab to webview for twa"
    This reverts commit 5015b97c7fd49208febc85a4af21d4775ad7838f.

--- android/twa/app/src/main/java/app/vercel/touchbasepro/twa/DelegationService.java
+++ android/twa/app/src/main/java/app/vercel/touchbasepro/twa/DelegationService.java
@@ -1,17 +1,22 @@
 package app.vercel.touchbasepro.twa;
 
 
-import android.content.Context;
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
 import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
 
-public class DelegationService extends com.google.androidbrowserhelper.trusted.DelegationService {
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
     @Override
     public void onCreate() {
         super.onCreate();
 
-        // Keep only billing functionality
-        Context context = getApplicationContext();
-        registerExtraCommandHandler(new DigitalGoodsRequestHandler(context));
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
+        
     }
 }
 
