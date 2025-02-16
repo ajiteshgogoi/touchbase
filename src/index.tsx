e5279f8c6a7febaf504c12e1b4060d8424a2b5d0
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-02-16 19:22:59 +0530

    Revert "Remove sw registrations for ios instagram"
    This reverts commit 96e5ebc50c4dfa96cdc3a622825a52a1d0f37a1f.

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { registerServiceWorker } from './registerSW';
-
-// Register service worker (will be skipped for iOS Instagram)
-registerServiceWorker();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
