2fb53196555b24544d426d6553b5bb11c36641f5
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-02-18 22:10:22 +0530

    Delete 20240218_update_content_reports_type.sql

--- supabase/migrations/20240218_update_content_reports_type.sql
+++
@@ -1,8 +0,0 @@
--- Drop existing content_type constraint
-ALTER TABLE public.content_reports
-DROP CONSTRAINT IF EXISTS content_reports_content_type_check;
-
--- Add new content_type constraint
-ALTER TABLE public.content_reports
-ADD CONSTRAINT content_reports_content_type_check 
-CHECK (content_type in ('suggestion', 'conversation-prompt'));
