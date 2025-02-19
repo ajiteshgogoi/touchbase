b60d668fbec21a1f51fc2190255e352d6d005ea2
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-02-19 20:04:28 +0530

    Revert "Remove manual chunking"
    This reverts commit eab7a93b46f71a64393b0ca12befa48d10cc14c8.

--- vite.config.ts
+++ vite.config.ts
@@ -135,6 +135,19 @@ export default defineConfig({
         generatedCode: {
           preset: 'es2015',
           symbols: false
+        },
+        manualChunks(id) {
+          // Group react runtime together
+          if (id.includes('node_modules/react/') ||
+              id.includes('node_modules/react-dom/') ||
+              id.includes('node_modules/scheduler/')) {
+            return 'react-runtime';
+          }
+          // Group major UI dependencies
+          if (id.includes('node_modules/@headlessui/') ||
+              id.includes('node_modules/@heroicons/')) {
+            return 'ui-libs';
+          }
         }
       }
     }
