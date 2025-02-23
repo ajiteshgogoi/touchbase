69705ce3bada3c41b22a8e5378ee268251cf2056
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-02-24 01:04:56 +0530

    Update QuickReminderModal.tsx

--- src/components/reminders/QuickReminderModal.tsx
+++ src/components/reminders/QuickReminderModal.tsx
@@ -157,7 +157,7 @@ const QuickReminderModal = ({ isOpen, onClose }: QuickReminderModalProps) => {
                     </label>
                     <div className="space-y-2">
                       <div className="relative">
-                        <MagnifyingGlassIcon className="pointer-events-none absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
+                        <MagnifyingGlassIcon className="pointer-events-none absolute top-1/2 left-3 -translate-y-1/2 h-5 w-5 text-gray-400" />
                         <input
                           type="text"
                           placeholder="Search contacts..."
