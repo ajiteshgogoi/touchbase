72757298e3cc0c85d29c4da91037ee2de88ca7e6
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-03-05 18:20:21 +0530

    Reapply "Dont drop console for prod. for debug"
    This reverts commit 47c17e9cd5e06c6974b762fd407b6389ae3ebd80.

--- vite.config.ts
+++ vite.config.ts
@@ -143,7 +143,7 @@ export default defineConfig({
     },
     terserOptions: {
       compress: {
-        drop_console: true,
+        drop_console: false,
         ecma: 2020,
         passes: 3,
         pure_getters: true,
@@ -156,7 +156,6 @@ export default defineConfig({
         inline: 3,
         reduce_vars: true,
         reduce_funcs: true,
-        pure_funcs: ['console.log', 'console.debug', 'console.info'],
         sequences: true
       },
       mangle: {
