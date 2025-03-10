c08077ac6e5c67167f90c7d2fd469d2824f0730e
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-03-11 00:55:07 +0530

    Update AdvancedContactInfo.tsx

--- src/components/contacts/AdvancedContactInfo.tsx
+++ src/components/contacts/AdvancedContactInfo.tsx
@@ -495,7 +495,7 @@ export const AdvancedContactInfo = ({
                 <li>Details about their family</li>
                 <li>Conversation topics they enjoy</li>
                 <li>Shared memories or inside jokes</li>
-                <li>Use upto 5 hashtags to filter and find contacts later</li>
+                <li>Hashtags act as contact categories. You can use upto 5 hashtags.</li>
                 <li>Start hashtags with # followed by a letter (max 15 characters, letters/numbers/underscore only)</li>
               </ul>
             </div>
