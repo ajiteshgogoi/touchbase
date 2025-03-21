6b26426d4d95ce968648eba1bc47acdb9cef6b3b
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-03-21 08:38:20 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@
 function addCorsHeaders(headers = new Headers()) {
   headers.set('Access-Control-Allow-Origin', '*');
   headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  headers.set('Access-Control-Allow-Headers', 'authorization, x-client-info, apikey, content-type');
+  headers.set('Access-Control-Allow-Headers', 'authorization, x-client-info, apikey, content-type, x-client-secret');
   headers.set('Access-Control-Max-Age', '86400');
   headers.set('Access-Control-Allow-Credentials', 'true');
   return headers;
