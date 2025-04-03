946cf261fe95380535eadf31448d2d841a495409
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-04-03 07:05:02 +0530

    Fix errors

---
+++ sanity/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './schemaTypes/**/*.{js,ts,jsx,tsx}',
+    './*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
