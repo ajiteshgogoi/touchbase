b2fac6483868bdf117e057ac5699bedbde97f3ea
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-04-04 10:43:50 +0530

    Add sanity studio branding

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,18 @@
+/** @jsx React.createElement */
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import React, { FC } from 'react'
+
+// Custom logo component
+const Logo: FC = () => {
+  return React.createElement('img', {
+    src: 'favicon.svg',
+    alt: 'TouchBase',
+    style: { height: '2em' }
+  })
+}
 
 export default defineConfig({
   name: 'default',
@@ -9,6 +20,12 @@ export default defineConfig({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID || '',
   dataset: process.env.SANITY_STUDIO_DATASET || 'production',
   basePath: '/studio', // Mount Sanity Studio at /studio
+
+  studio: {
+    components: {
+      logo: Logo
+    }
+  },
 
   plugins: [structureTool(), visionTool()],
 
