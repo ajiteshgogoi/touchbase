5dadae37d4b2b728054aa36bbed68c83736c1357
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-04-30 10:31:06 +0530

    Update index.ts

--- supabase/functions/get-user-stats/index.ts
+++ supabase/functions/get-user-stats/index.ts
@@ -147,7 +147,8 @@ serve(async (req) => {
       'https://lh3.googleusercontent.com/a/ACg8ocJzGyCHWKdtXvTvldLSL184B7zwOU1lXziQBMw5-zZMSU21naI=s96-c',
       'https://lh3.googleusercontent.com/a/ACg8ocJK-BQFKR5yIfr-IWh1zIWq74Nh0Wmdo4191s3hHR5QqLFoug=s96-c',
       'https://lh3.googleusercontent.com/a/ACg8ocLkxma8DpxT0ZII7LPtceYEKCqE9YqIxnbQ7C3Lh-mm6qEgPw=s96-c',
-      'https://lh3.googleusercontent.com/a/ACg8ocLEGOYX0m6p-YNwnDboJaBiggupM7yasXKXOiehw0TpPy2B6RJA=s96-c'
+      'https://lh3.googleusercontent.com/a/ACg8ocLEGOYX0m6p-YNwnDboJaBiggupM7yasXKXOiehw0TpPy2B6RJA=s96-c',
+      'https://lh3.googleusercontent.com/a/ACg8ocKGHBten26TJXgsfxcO73vvF-wcP2QzSSXeSFIInEdIjQ8bQg=s96-c'
 
 
     ];
